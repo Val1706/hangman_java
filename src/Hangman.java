e70f2f09a16818bc004abaac65d01fd782fee2ac
Val1706 <potapovvalentine@gmail.com>
2017-08-02 03:58:32 +0200

    Update Hangman.java

--- src/Hangman.java
+++ src/Hangman.java
@@ -2,34 +2,62 @@ import java.util.*;
 
 public class Hangman {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         String country = "Poland".toUpperCase();
         List country_with_dash = make_dashboard(country);
 
-        while(true) {
+        int bad_quess = 0;
+        int all_quesses = 6;
+
+        List<String> used_letters = new ArrayList<String>();
+
+        boolean success;
+        success = false;
+
+        while (!success && bad_quess < all_quesses) {      
 
             System.out.println("Please type a letter: ");
             Scanner scan = new Scanner(System.in);
             String quess = scan.next().toUpperCase();
 
-            if (quess.equals(country)) {
-                break;
 
-            }else {
-                if (quess.matches("[A-Za-z]{1}")){
+            if (quess.equals(country)) { 
+                // check if user quessed country at once
+                success = true;
+            }
 
+            if (quess.matches("[A-Za-z]{1}") && !used_letters.contains(quess)) { 
+                // check if user provided appropriate letter
+
+                if (country.contains(quess)) {
+                    // check if user provided letter which is in word
+                    
                     List added_letters_to_country = set_new_letters(country_with_dash, quess, country);
 
                     String country_with_new_letters = show_updated_dash_word(added_letters_to_country);
                     System.out.println(country_with_new_letters);
 
                     if (country_with_new_letters.equals(country)) {
-                        break;
+                        // check if user provided all letters and quessed country
+                        success = true;
                     }
 
+                }else {
+
+                    bad_quess++;
+                    System.out.println("Wrong!");
+                }
+
+            }else {
+                if (used_letters.contains(quess)) {
+                    // check if user has already provided the same letter
+                    System.out.println("You have already used this word");
+                } else{
+                    System.out.println("It is not " + quess);
                 }
             }
+            used_letters.add(quess);
         }
     }
 
@@ -63,5 +91,4 @@ public class Hangman {
 
         return new_dash.toUpperCase();
     }
-
 }
