82c9c177499638307a10ad61d9a8670217b7c2b4
Val1706 <potapovvalentine@gmail.com>
2017-08-06 23:06:17 +0200

    Update Controller.java

--- src/Controller.java
+++ src/Controller.java
@@ -5,28 +5,26 @@ public class Controller {
 
     public static void start_controller() {
 
-        Hangman hangman = Database.get_hangman_object();
+        while (true) {
 
-        List country_with_dash = hangman.make_dashboard(hangman.capital);
+            Hangman hangman = Database.get_hangman_object();
 
-        List<String> used_letters = new ArrayList<String>();
+            List country_with_dash = hangman.make_dashboard(hangman.capital);
+            showing_dashboard(hangman, country_with_dash);
 
-        int bad_quesses = 0;
-        boolean success = false;
+            List<String> used_letters = new ArrayList<String>();
 
+            int bad_quesses = 0;
 
-        while (!success && bad_quesses < 6) {
+            boolean success = false;
+            boolean finish_game = true;
 
-            String quess = View.get_question();
+            System.out.println(hangman.capital);
 
-            if (quess.equals(hangman.capital)) {
-                // check if user quessed country at once
-                success = true;
+            while (finish_game) {
 
-            } else {
-                if (bad_quesses == 4) {
-                    View.show_hint_country(hangman.country);
-                }
+                String quess = View.get_question();
+
                 if (quess.matches("[A-Za-z]{1}") && !used_letters.contains(quess)) {
                     // check if user provided appropriate letter
 
@@ -34,32 +32,59 @@ public class Controller {
                         // check if user provided letter which is in word
 
                         List added_letters_to_country = hangman.set_new_letters(country_with_dash, quess, hangman.capital);
-
-                        String country_with_new_letters = hangman.show_updated_dash_word(added_letters_to_country);
-                        View.show_country(country_with_new_letters);
+                        String country_with_new_letters = showing_dashboard(hangman, added_letters_to_country);
 
                         if (country_with_new_letters.equals(hangman.capital)) {
                             // check if user provided all letters and quessed country
                             success = true;
                         }
-                    }else{
+                    } else {
                         View.no_letter_msg();
                         bad_quesses++;
                     }
                     used_letters.add(quess);
-                }else {
+
+                } else {
+
                     if (used_letters.contains(quess)) {
                         // check if user has already provided the same letter
                         View.repeat_letter();
-                    }else{
+                    } else if (quess.equals(hangman.capital)) {
+                        success = true;
+                    } else {
                         View.no_letter_msg();
-                        bad_quesses ++;
-                        }
+                        bad_quesses++;
                     }
-
                 }
+                if (success) {
+                    System.out.println("You won");
+                    finish_game = false;
 
+                } else if (bad_quesses == 3) {
+                    View.show_hint_country(hangman.country);
+
+                } else if (bad_quesses == 5) {
+
+                    System.out.println("You lost");
+                    boolean answer = View.play_again();
+
+                    if (answer) {
+                        finish_game = false;
+                    } else {
+                        System.exit(0);
+
+                    }
+                }
             }
         }
     }
+
+    private static String showing_dashboard(Hangman hangman, List added_letters_to_country) {
+
+        String country_with_new_letters = hangman.show_dash_word(added_letters_to_country);
+        View.show_country(country_with_new_letters);
+
+        return country_with_new_letters;
+    }
+}
 
